406fe93d45d17bebc436c37387f52b90ec94d259
rthulasi88 <thulasi.rajakannu@saintmichelannecy.fr>
2023-04-17 09:25:59 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "todos",
+    },
+    listPerPage: 10,
+};
+module.exports = config;
